c726833c4852400dfe89e63c4c064c34725d22ae
mdavid5 <mcdav3636@gmail.com>
2017-12-07 20:52:10 -0700

    started page

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>K'lynn's</title>
+		<!-- required meta tags -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- link to css stylesheet -->
+		<link rel="stylesheet" href="style/style.css" type="text/css"/>
+
+		<!-- link to jQuery stylesheet -->
+
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- font awesome -->
+		<script src="https://use.fontawesome.com/02efcb04d1.js"></script>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="scripts/jquery-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- java script link -->
+		<script src="scripts/main.js"></script>
+
+	</head>
+
+	<header>
+
+	</header>
+
+	<body>
+
+
+		<!-- content section -->
+		<div class="container">
+			  <div class="row">
+				  <div class="col-12-sm">
+					  <div class="welcome">
+						  <img class="image-fluid" src="./images/klynns-food.jpg" alt="image of shrimp dish">
+					  </div>
+				  </div>
+			  </div>
+				<!--navigation bar -->
+				<nav class="navbar fixed-bottom navbar-default navbar-expand-md bg-transparent">
+					<a class="navbar-brand" href="index.php">K'Lynn's Souther and Cajun</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbar TogglerDemo01">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#" id="">Dining</a>
+							</li>
+							<li>
+								<a class="nav-link" href="#" id="">Catering</a>
+							</li>
+							<li>
+								<a class="nav-link" href="#" id="">Menu</a>
+							</li>
+							<li>
+								<a class="nav-link" href="#" id="">About</a>
+							</li>
+							<li>
+								<a class="nav-link" href="#" id=""></a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+		</div>
+	</body>
